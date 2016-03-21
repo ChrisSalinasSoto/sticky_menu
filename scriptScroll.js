07664e4aff8a927491d155793ea27bf665bfa4fb
ChrisSalinasSoto <SSSillyMac@MacBook-Pro-van-Christofer.local>
2016-03-21 11:30:06 +0100

    klaar

---
+++ scriptScroll.js
@@ -0,0 +1,20 @@
+var scrollValue;
+var lastOne = 0
+var nav = $('nav');
+var height = nav.outerHeight();
+
+function verplaats() {
+    scrollValue = $(this).scrollTop();
+    if(scrollValue > lastOne) {
+        console.log('we scrollen naar beneden');
+        lastOne=scrollValue;
+        nav.css('top', -height + 'px')
+    } else {
+        lastOne=scrollValue;
+        nav.css('top', 0);
+    }
+    
+   
+}
+
+$(window).on('scroll', verplaats)
